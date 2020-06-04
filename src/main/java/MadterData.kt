2f368a8ea383b9b1b6ea39ca9b2518c9a3a70f09
Youhei Oonuma <oonuma617@gmail.com>
2020-06-04 21:38:27 +0900

    update master data

--- src/main/java/MadterData.kt
+++ src/main/java/MadterData.kt
@@ -1,19 +1,19 @@
 val members = """
-ときのそら	tokino_sora 0
-ロボ子さん	robocosan   0
-さくらみこ   sakuramiko35    0
-白神フブキ	shirakamifubuki 1,G
-夏色まつり   natsuiromatsuri    1
-赤井はあと	akaihaato   1
-湊あくあ	minatoaqua  2
-大空スバル oozorasubaru  2
-犬神ミオ    ookamimio   G
-猫又おかゆ	nekomataokayu   G
-犬神ころね	inugamikorone   G
-宝鐘マリン   houshoumarine   3
-兎田ぺこら	usadapekora 3
-潤羽るしあ	uruharushia 3
-星街すいせい  suisei_hosimati 0
-桐生ココ	kiryucoco   4
-天音かなた	amanekanatach   4
+ときのそら	tokino_sora 1
+ロボ子さん	robocosan   1
+さくらみこ   sakuramiko35    1
+星街すいせい  suisei_hosimati 1
+白神フブキ	shirakamifubuki 1,4
+夏色まつり   natsuiromatsuri    2
+赤井はあと	akaihaato   2
+湊あくあ	minatoaqua  3
+大空スバル oozorasubaru  3
+犬神ミオ    ookamimio   4
+猫又おかゆ	nekomataokayu   4
+犬神ころね	inugamikorone   4
+宝鐘マリン   houshoumarine   5
+兎田ぺこら	usadapekora 5
+潤羽るしあ	uruharushia 5
+桐生ココ	kiryucoco   6
+天音かなた	amanekanatach   6
 """
